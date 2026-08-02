05856d1cf9a5c8d2d63eaef1b3591be9c0b1d38f
lintrix <lintrix@Lintrix.lintrix.com>
2026-08-02 17:29:36 +0300

    Manage dash data miss resol

--- src/models/TuneModel.js
+++ src/models/TuneModel.js
@@ -485,7 +485,7 @@ class TuneModel {
   // File operations
   async downloadTune(id) {
     try {
-      const response = await tuneAPI.download(id);
+      const response = await tuneAPI.downloadBlob(id);
       return response.data || response;
     } catch (error) {
       throw new Error(`Failed to download tune: ${error.message}`);
